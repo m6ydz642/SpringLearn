8f14024607b92a36838b3a74be5b874baefbf40c
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2020-11-30 03:17:13 +0900

    클래스 추가

---
+++ sp5-chap02/src/main/java/chap02/Gretter.java
@@ -0,0 +1,14 @@
+package chap02;
+
+public class Gretter {
+	private String format;
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+}
