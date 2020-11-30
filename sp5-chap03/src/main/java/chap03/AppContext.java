c275b30cac98168dae93128f17e561dc1eb63e95
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2020-11-30 22:24:16 +0900

    cp03추가

---
+++ sp5-chap03/src/main/java/chap03/AppContext.java
@@ -0,0 +1,24 @@
+package chap03;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 스프링 설정 클래스로 지정
+public class AppContext {
+
+	@Bean // 스프링이 관리하는 빈객체
+	public Gretter gretter() { 
+		Gretter g = new Gretter();
+		g.setFormat("%s 님 안녕");
+		return g;
+		
+	}
+	
+	@Bean // 스프링이 관리하는 빈객체
+	public Gretter gretter2() { // gretter2이름으로 객체 한개 더 생성 
+		Gretter g = new Gretter();
+		g.setFormat("%s 님 안녕? (2번째 객체) ");
+		return g;
+		
+	}
+}
---
+++ sp5-chap03/src/main/java/chap03/Gretter.java
@@ -0,0 +1,15 @@
+package chap03;
+
+public class Gretter {
+	private String format;
+
+	public String getFormat(String guest) {
+		// System.out.println("호출된 aurgument : " + String.format(format));
+		return String.format(format, guest);
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+}
