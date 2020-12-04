ae4cae089a146d7eb97bbf188828e433c9372070
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2020-12-04 15:55:10 +0900

    주석처리

--- sp5-chap02/src/main/java/chap02/Main.java
+++ sp5-chap02/src/main/java/chap02/Main.java
@@ -7,9 +7,13 @@ public class Main {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
 
-		Gretter g = ctx.getBean("gretter",Gretter.class);
+		Gretter g = ctx.getBean("gretter",Gretter.class); // 여기서 설정된 gretter는 
+											// appcontext에 있는 gretter객체타입 함수
+		// g변수에는 %s 님안녕 이라는 내용이 이미 들어간 상태로 getBean으로 호출 됨
 		String msg = g.getFormat("스프링");
 		System.out.println(msg);
+		// 출력결과는 %s 님 안녕
+		// getFormat함수에서 인자 반대로 해서 printf문 처럼 호출해주는 String.format함수가 되어 있음
 		
 	/*	Gretter g1 = ctx.getBean("gretter",Gretter.class); // 별도 설정이 없을경우 스프링은 한 개의 빈객체만을 생성
 															// 싱글톤 범위임
