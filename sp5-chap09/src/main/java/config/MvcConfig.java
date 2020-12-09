3366fe881ff5f6be61846be31109088b26825666
m6ydz642 <m6ydz642@hotmail.com>
2020-12-09 23:29:47 +0900

    Spring MVC 설정, web.xml에 디스패쳐 서블릿 설정

--- sp5-chap09/src/main/java/config/MvcConfig.java
+++ sp5-chap09/src/main/java/config/MvcConfig.java
@@ -1,14 +1,28 @@
 package config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-@EnableWebMvc
+@EnableWebMvc // 스프링 MVC설정 활성화
 
-public class MvcConfig implements WebMvcConfigurer{
+public class MvcConfig implements WebMvcConfigurer{ // MVC개별설정을 조정할 때 사용
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+		//  디스패쳐서블릿의 매핑 경로를 /로 주었을 때, JSP/HTML/CSS등을 올바르게 처리하기 위한 설정
+		configurer.enable();
+		
+	}
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registy){
+		// JSP를 이용해서 컨트롤러의 실행 결과를 보여주기 위한 설정 추가
+		registy.jsp("/WEB-INF/view/", ".jsp");
+	}
 	
 	
 
