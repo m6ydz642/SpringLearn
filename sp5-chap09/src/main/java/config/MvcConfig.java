90144a0974b334df20b2516e1fb1c86a73859ba0
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2020-12-09 15:38:22 +0900

    pom.xml추가, WEB-INF 위치 변경

---
+++ sp5-chap09/src/main/java/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+
+public class MvcConfig implements WebMvcConfigurer{
+	
+	
+
+
+}
