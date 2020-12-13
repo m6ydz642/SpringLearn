8d16bde7bcc767df8ec87878795d234eb3afb75d
m6ydz642 <m6ydz642@gmail.com>
2020-12-13 22:07:06 +0900

    요청파라미터 접근

--- sp5-chap11/src/main/java/controller/RegisterController.java
+++ sp5-chap11/src/main/java/controller/RegisterController.java
@@ -1,7 +1,9 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegisterController {
@@ -12,10 +14,17 @@ public class RegisterController {
 		return "register/step1";
 	}
 	
-	public String handleStep2(){ // 준비중
-		return null;
-		
-	}
+	@PostMapping("/register/step2")
+	public String handleStep2(@RequestParam(value="agree", defaultValue="false")
+	Boolean agreeVal) { // agree요청 값을 읽어와 boolean타입으로 변환해서 agreeval파라메터에 전달함
+		System.out.println("register step2 타입 결과 : " + agreeVal);
+		if(!agreeVal){
+			System.out.println("register step2 타입 결과 : " + agreeVal);
+			return "register/step1";
+		}
+
+		return "register/step2";		
+}
 	
 public String handleStep3(){ // 준비중
 	return null;
