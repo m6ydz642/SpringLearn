7c12418fb314e0018f4fa37a0fafb8f01d17aecd
m6ydz642 <m6ydz642@gmail.com>
2020-12-13 22:25:31 +0900

    alert추가

--- sp5-chap11/src/main/java/controller/RegisterController.java
+++ sp5-chap11/src/main/java/controller/RegisterController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +21,29 @@ public class RegisterController {
 	
 	@PostMapping("/register/step2")
 	public String handleStep2(@RequestParam(value="agree", defaultValue="false")
-	Boolean agreeVal) { // agree요청 값을 읽어와 boolean타입으로 변환해서 agreeval파라메터에 전달함
+	Boolean agreeVal, HttpServletResponse response) { // agree요청 값을 읽어와 boolean타입으로 변환해서 agreeval파라메터에 전달함
 		System.out.println("register step2 타입 결과 : " + agreeVal);
+
 		if(!agreeVal){
+			try {
+				
+				response.setContentType("text/html; charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				out.println("<script>alert('가입 실패'); location.href='/sp5-chap11/register/step1';</script>");
+				out.flush();
+				
+			} catch (IOException e) {
+				System.out.println("예외 발생 : " + e);
+				e.printStackTrace();
+			}
+			
 			System.out.println("register step2 타입 결과 : " + agreeVal);
-			return "register/step1";
+			return "register/step1";// 위에서 리다이렉션 해줘서 사실 필요없음	
+		}else{
+			return "register/step2";	
 		}
 
-		return "register/step2";		
+				
 }
 	
 public String handleStep3(){ // 준비중
